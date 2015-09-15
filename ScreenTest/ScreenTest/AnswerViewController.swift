13bf28bb63fb2e90ab85a79c692ab9940cc58e56
Andrea Ferrari <andreaferrari0926@yahoo.com>
2015-09-15 14:59:17 +0200

    Implemented view controllers for the first tab

--- ScreenTest/ScreenTest/AnswerViewController.swift
+++ ScreenTest/ScreenTest/AnswerViewController.swift
@@ -6,4 +6,30 @@
 //  Copyright (c) 2015 Andrea Ferrari. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AnswerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
--- ScreenTest/ScreenTest/TestOverviewViewController.swift
+++ ScreenTest/ScreenTest/TestOverviewViewController.swift
@@ -6,4 +6,68 @@
 //  Copyright (c) 2015 Andrea Ferrari. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MediaPlayer
+
+protocol TestOverViewControllerDelegate {
+    func onNextStepButtonClicked(controller:TestOverviewViewController)
+}
+
+class TestOverviewViewController: UIViewController {
+    
+    var delegate:TestOverViewControllerDelegate! = nil
+    
+    @IBOutlet weak var vwVideo: UIView!
+    @IBOutlet weak var btnNextStep: UIButton!
+    
+    var moviePlayer : MPMoviePlayerController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.playVideo()
+        BNUIManager.applyDefaultButtonStyle(self.btnNextStep)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func onNextStepButtonClicked(sender: UIButton) {
+        if( self.delegate != nil ) {
+            self.delegate.onNextStepButtonClicked(self)
+        }
+    }
+    
+    func playVideo() {
+        let url = NSURL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = moviePlayer {
+            player.view.backgroundColor = UIColor.whiteColor()
+            self.vwVideo.layoutIfNeeded()
+            player.view.frame = self.vwVideo.bounds
+            player.controlStyle = MPMovieControlStyle.Fullscreen;
+            player.prepareToPlay()
+            player.scalingMode = .AspectFit
+            self.vwVideo.addSubview(player.view)
+        } else {
+            debugPrintln("Something wrong when playing video")
+        }
+        
+        
+    }
+    
+}
