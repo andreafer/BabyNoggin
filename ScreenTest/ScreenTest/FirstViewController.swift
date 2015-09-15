f2381728a3c238e1e56c66354570ef768d4ff007
Andrea Ferrari <andreaferrari0926@yahoo.com>
2015-09-15 15:00:45 +0200

    Implemented First View Controller

--- ScreenTest/ScreenTest/FirstViewController.swift
+++ ScreenTest/ScreenTest/FirstViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: BaseViewController, TestOverViewControllerDelegate {
+    
+    @IBOutlet weak var lblSubtitle: UILabel!
+    @IBOutlet weak var vwContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.showTestOverviewControllerWithAnimated(false)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func showTestOverviewControllerWithAnimated( animated: Bool ) {
+        var duration = 0.0
+        
+        if( animated ) {
+            duration = 0.2
+        }
+        
+        let vcTestOverview = storyboard!.instantiateViewControllerWithIdentifier("TestOverview") as! TestOverviewViewController
+        self.addChildViewController(vcTestOverview)
+        
+        vcTestOverview.view.frame = self.vwContainer.bounds
+        vcTestOverview.delegate = self;
+        vcTestOverview.view.alpha = 0;
+        self.vwContainer.addSubview(vcTestOverview.view)
+        
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            vcTestOverview.view.alpha = 1;
+        })
+    }
+    
+    func showTimeToTestViewControllerWitHAnimated( animated: Bool ) {
+        var duration = 0.0
+        
+        if( animated ) {
+            duration = 0.2
+        }
+        
+        let vcTimeToTest = storyboard!.instantiateViewControllerWithIdentifier("TimeToTest") as! TimeToTestViewController
+        self.addChildViewController(vcTimeToTest)
+        
+        vcTimeToTest.view.frame = self.vwContainer.bounds
+        vcTimeToTest.view.alpha = 0;
+        self.vwContainer.addSubview(vcTimeToTest.view)
+        
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            vcTimeToTest.view.alpha = 1;
+        })
+    }
+    
+    //TestOverViewController Delegate
+    func onNextStepButtonClicked(controller: TestOverviewViewController) {
+        controller.view.removeFromSuperview()
+        controller.removeFromParentViewController()
+        self.showTimeToTestViewControllerWitHAnimated(true);
+    }
 }
 
